03b9858636071eaa06b79826b23b85d7bfdf8c2a
slurpgoodsoup <sloonaperchonka@gmail.com>
2022-02-16 15:04:25 +0300

    конечная верстка

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ const App = () => {
   return (
     <div className="app-wrapper">
       <header className="header">
-        <img className = "header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png" />
+        <img
+          className="header__icon"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png"
+        />
       </header>
       <nav className="nav">
         <ul className="nav-list">
@@ -23,9 +26,6 @@ const App = () => {
           <li className="nav-list__el">
             <a href="#">Settings</a>
           </li>
-
-          
-
         </ul>
       </nav>
       <div className="main-content">
@@ -41,18 +41,32 @@ const App = () => {
 
           <div className="profile-desc">
             <p className="profile-desc__el">Ну да я</p>
-            <p className="profile-desc__el">
-              Date of birth: 2 Jan
-            </p>
-            <p className="profile-desc__el">
-              City: Moscow
-            </p>
-            <p className="profile-desc__el">
-              Education: OAO'22
-            </p>
-            <p className="profile-desc__el">
-              WebSite: oao.com
-            </p>
+            <p className="profile-desc__el">Date of birth: 2 Jan</p>
+            <p className="profile-desc__el">City: Moscow</p>
+            <p className="profile-desc__el">Education: OAO'22</p>
+            <p className="profile-desc__el">WebSite: oao.com</p>
+          </div>
+        </div>
+        <div className="posts">
+          <p className="posts__my-posts">My posts</p>
+          <div className="new-post">
+            <textarea
+              className="new-post__textarea"
+              placeholder="Ну че там.."
+            ></textarea>
+            <button className="new-post__btn">Send</button>
+          </div>
+          <div className="posts-list">
+            <div className="posts-list-el">
+              <img
+                className="posts-list-el__photo"
+                src="https://vjoy.cc/wp-content/uploads/2020/04/50059_19633152-7566413-image-a-56_1570908569851.jpg"
+              />
+              <p className="posts-list-el__name">Kotya</p>
+              <p className="posts-list-el__text">
+                Oh-oh-oh-oh-oh-oh-oh-oh-oh-oh-oh-oh Caught in a bad romance
+              </p>
+            </div>
           </div>
         </div>
       </div>
