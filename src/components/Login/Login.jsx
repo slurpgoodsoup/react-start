06fc3729422b04dc103aab7c9bf64bd9d5340d26
slurpgoodsoup <sloonaperchonka@gmail.com>
2022-07-18 16:52:49 +0300

    login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, yupToFormErrors } from "formik";
 import * as Yup from "yup";
-import { Field } from "react-final-form";
+import style from './Login.module.css'
 
 const Login = () => {
   return <div>
@@ -16,64 +16,68 @@ const LoginForm = () => {
     password: Yup.string().required('Required')
   })
 
-  return <Formik
-  initialValues = {{
-      email: '',
-      password: '',
-      rememberMe: false
-    }}
-    validateOnBlur
-    onSubmit={(value) => {console.log(value)}}
-    validationSchema = {validationSchema}
+  return <div className={style.loginPage}>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        rememberMe: false
+      }}
+      validateOnBlur
+      onSubmit={(value) => console.log(value)}
+      validationSchema = {validationSchema}
     >
-
-      {({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty}) => (
+      {({values, errors, touched, handleChange, handleBlur, handleSubmit, dirty, isValid})=> (
         <div>
 
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-            type={'email'}
-            name={'email'}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-            />
-              {touched.email && errors.email && <p>{errors.email}</p>}
-          </div>
+        <div className={style.input}>
+          <label>Email </label>
+          <input
+          name={'email'}
+          type={'email'}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+          />
+          {touched.email && errors.email && <p className={style.error}>{errors.email}</p>}
+        </div>
+        <div className={style.input}>
+          <label>Password </label>
+          <input
+          name={'password'}
+          type={'password'}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          />
+          {touched.password && errors.password && <p className={style.error}>{errors.password}</p>}
+        </div>
+        <div className={style.input}>
+          <input
+          name={'rememberMe'}
+          type={'checkbox'}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.checkbox}
+          />
+          <label>RememberMe</label>
+        </div>
 
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-            type={'password'}
-            name={'password'}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-            />
-              {touched.password && errors.password && <p>{errors.password}</p>}
-          </div>
+        <div>
+          <button
+          className={style.submitBtn}
+          type = {'submit'}
+          disabled = {!isValid && !dirty}
+          onClick={handleSubmit}
+          >Submit</button>
+        </div>
 
-          <div>
-            <input
-            type={'checkbox'}
-            name={'rememberMe'}
-            value={values.rememberMe}
-            onChange={handleChange}/>
-            <label htmlFor="rememberMe">Remember me</label>
-          </div>
-
-          <div>
-            <button
-            type = {'submit'}
-            disabled = {!isValid && !dirty}
-            onClick = {handleSubmit}
-            >Submit</button>
-          </div>
         </div>
       )}
 
   </Formik>
+ </div>
+
 }
 
 
